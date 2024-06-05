2944121b294df819ca637d89056fee589542e3ac
devprinceng <devprinceng@gmail.com>
2024-06-05 15:55:02 +0100

    added mongodb conection string

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,16 @@
+const e = require("express");
 const express = require("express");
+const { default: mongoose } = require("mongoose");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+
+// mongodb connection
+mongoose
+    .connect('mongodb+srv://remotedev0:2cuaZREuNGa9jOZN@devprince-mern-journey.degdoxp.mongodb.net/devprince-mern-journey?retryWrites=true&w=majority&appName=devprince-mern-journey')
+    .then(() => console.log("DB Connected"))
+    .catch((e) => console.log(e))
 
 // start server
 app.listen(PORT, console.log(`Server is running at Port ${PORT}`))
