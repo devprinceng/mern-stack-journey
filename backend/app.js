bd7988dcbc572ba04c7de2d636ae7d649623ce8f
devprinceng <devprinceng@gmail.com>
2024-06-07 02:58:57 +0100

    code updated

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,3 @@
-const e = require("express");
 const express = require("express");
 const { default: mongoose } = require("mongoose");
 const app = express();
@@ -8,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 
 // mongodb connection
 mongoose
-    .connect('mongodb+srv://remotedev0:2cuaZREuNGa9jOZN@devprince-mern-journey.degdoxp.mongodb.net/devprince-mern-journey?retryWrites=true&w=majority&appName=devprince-mern-journey')
+    .connect()
     .then(() => console.log("DB Connected"))
     .catch((e) => console.log(e))
 
