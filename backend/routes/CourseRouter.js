15886d71dfe005883b3235c7e2945d4ecfab0ded
devprinceng <devprinceng@gmail.com>
2024-06-12 16:11:12 +0100

    added course update endpoint

--- backend/routes/CourseRouter.js
+++ backend/routes/CourseRouter.js
@@ -9,5 +9,6 @@ const courseRouter = express.Router();
 courseRouter.post('/api/v1/courses/create', isAuthenticated, courseController.create) //create course route
 courseRouter.get('/api/v1/courses/lists', courseController.list) //course lists
 courseRouter.get('/api/v1/courses/:courseId', courseController.getCourseById) //find ourse by ID
+courseRouter.put('/api/v1/courses/:courseId', courseController.update) //find ourse by ID
 
 module.exports = courseRouter;
